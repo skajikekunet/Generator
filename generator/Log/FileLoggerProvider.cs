f715f98787d63586d513f5bd8ade377aaac8c877
skajikekunet <johnhill1612@gmail.com>
2021-07-05 13:20:16 +0500

    git log

---
+++ generator/Log/FileLoggerProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace generator.Log
+{
+    public class FileLoggerProvider : ILoggerProvider
+    {
+        private string path;
+        public FileLoggerProvider(string _path)
+        {
+            path = _path;
+        }
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new FileLogger(path);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
